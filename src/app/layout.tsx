0f847925afab24c940ca8e3287207c41a98399bd
mjwmwetzer <marko.wetzer@gmail.com>
2025-07-09 15:54:04 +0200

    Fix hydration mismatch error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="nl">
+    <html lang="nl" suppressHydrationWarning={true}>
       <body className="bg-gray-100 min-h-screen" suppressHydrationWarning={true}>
         {children}
       </body>
